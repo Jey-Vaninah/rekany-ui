3d5851782e5ca60a8c5a7a546a9c95ea65f9eaca
Jey-Vaninah <hei.vaninah@gmail.com>
2026-07-27 12:47:37 +0300

    feat: home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,16 @@
-import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import NotFound404 from "./components/NotFound404";
-
-
+import HomePage from "./pages/Home/Home";
 
 export default function App() {
   return (
-    <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />} >
-            {/* ovaina ny route nareo ny eto */}
-            <Route path='/' element={<p>Home</p>} />  
-            {/* <Route path="/à-propos" element={<p>About</p>} /> */}
-            {/* <Route path="/services" element={<p>Services</p>} /> */}
-            {/* <Route path="/contact" element={<p>Contact</p>} /> */}
-          </Route>
-          <Route path="*" element={<NotFound404 />} />
-        </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<HomePage />} />
+      </Route>
+
+      <Route path="*" element={<NotFound404 />} />
+    </Routes>
   );
 }
